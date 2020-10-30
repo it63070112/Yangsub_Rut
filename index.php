0b2b89dcb6dc461c9c8a6a6d74b2e49d279c1035
it63070112 <73678816+it63070112@users.noreply.github.com>
2020-10-30 15:55:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <title>Pornpaisn</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 <body>
-  <h1>พรไพศาล ยังทรัพย์ รุต</h1>
-  <img src="raf.jpg">
-  <img src="Foot.jpg">
-  <a href="https://www.youtube.com/watch?v=LV1Es22E0RI"></a>
-  <p3>คนไทย สุดยอด ไม่แพ้ชาติใดในโลก
-    ไฟไหม้ผับแห่งหนึ่งในยุโรป ที่ประตูทางออก
-    พบศพจำนวนมาก นักข่าวถามหาผู้รอดชีวิต
-    กลุ่มเดียวที่เป็นคนไทยว่ารอดมาได้ไง
-    คำตอบคือ ก็ออกตรงทางเข้าไง โคตรโล่งเลย</p3>
-</body>
-</html>
+    <h1>พรไพศาล ยังทรัพย์ รุต</h1>
+    <img src="raf.jpg">
+    <img src="Foot.jpg">
+    <a href="https://www.youtube.com/watch?v=LV1Es22E0RI"></a>
+    <p3>คนไทย สุดยอด ไม่แพ้ชาติใดในโลก
+        ไฟไหม้ผับแห่งหนึ่งในยุโรป ที่ประตูทางออก
+        พบศพจำนวนมาก นักข่าวถามหาผู้รอดชีวิต
+        กลุ่มเดียวที่เป็นคนไทยว่ารอดมาได้ไง
+        คำตอบคือ ก็ออกตรงทางเข้าไง โคตรโล่งเลย</p3>
+
+    </body>
+    </html>
